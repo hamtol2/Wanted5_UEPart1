1b9cf93d7bd8dea5d1aafcf4b5fc962bb6b6c094
ronniej <ronniej@naver.com>
2026-09-23 15:58:05 +0900

    컴포지션 실습 진행

--- Source/UEPart1/Person.h
+++ Source/UEPart1/Person.h
@@ -4,6 +4,9 @@
 #include "UObject/Object.h"
 #include "Person.generated.h"
 
+// 전방선언.
+class UCard;
+
 UCLASS()
 class UEPART1_API UPerson : public UObject
 {
@@ -13,11 +16,18 @@ public:
 	UPerson();
 
 	// Getter/Setter.
-	inline const FString& GetName() const { return Name; }
-	inline void SetName(const FString& InName) { Name = InName; }
+	FORCEINLINE const FString& GetName() const { return Name; }
+	FORCEINLINE void SetName(const FString& InName) { Name = InName; }
+
+	FORCEINLINE UCard* GetCard() const { return Card; }
+	FORCEINLINE void SetCard(UCard* InCard) { Card = InCard; }
 
 protected:
 	// 관리 받는 속성으로 선언.
 	UPROPERTY()
 	FString Name;
+
+	// 카드 객체.
+	UPROPERTY()
+	TObjectPtr<UCard> Card;
 };
--- Source/UEPart1/Student.cpp
+++ Source/UEPart1/Student.cpp
@@ -2,11 +2,15 @@
 
 
 #include "Student.h"
+#include "Card.h"
 
 UStudent::UStudent()
 {
 	// 값 설정.
 	Name = TEXT("학생");
+
+	// 카드 타입 설정.
+	Card->SetCardType(ECardType::Student);
 }
 
 void UStudent::DoLesson()
